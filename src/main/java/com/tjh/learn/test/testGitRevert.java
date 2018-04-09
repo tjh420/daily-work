ee61fd01cd4d2e4b350a002dba3daebe10a6681e
jhtao <jhtao@chinaums.com>
2018-04-09 10:23:12 +0800

    测试结果:idea中git的revert selected changes可以对单个文件向上个版本回退，nice

--- src/main/java/com/tjh/learn/test/testGitRevert.java
+++ src/main/java/com/tjh/learn/test/testGitRevert.java
@@ -6,6 +6,5 @@ package com.tjh.learn.test;
 public class testGitRevert {
     public static void main(String[] args) {
         System.out.println("version1");
-        System.out.println("version2");
     }
 }
