d5e7b6c9b4b18f9a8941ab7aa89c43851ad75790
jhtao <jhtao@chinaums.com>
2019-03-06 11:52:42 +0800

    请求是一个list时应该采用第二种方式

---
+++ json/src/main/java/com/tjh/gsonFormat/Request.java
@@ -0,0 +1,45 @@
+package com.tjh.gsonFormat;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by tjh on 2019/3/6.
+ * [
+ *     {
+ *         "amount": "100.00",
+ *         "checkCode": "",
+ *         "invoiceCode": "1234567890",
+ *         "invoiceNo": "12345678",
+ *         "paperDrewDate": "20170101",
+ *         "yzLevel": "A",
+ *         "tenantCode": "sgcc",
+ *         "customerNo": "123"
+ *     },
+ *     {
+ *         "amount": "200.00",
+ *         "checkCode": "",
+ *         "invoiceCode": "1234567891",
+ *         "invoiceNo": "12345679",
+ *         "paperDrewDate": "20170201",
+ *         "yzLevel": "A",
+ *         "tenantCode": "sgcc",
+ *         "customerNo": "123"
+ *     }
+ * ]
+ */
+@NoArgsConstructor
+@Data
+public class Request {
+
+    private String amount;
+    private String checkCode;
+    private String invoiceCode;
+    private String invoiceNo;
+    private String paperDrewDate;
+    private String yzLevel;
+    private String tenantCode;
+    private String customerNo;
+
+
+}
---
+++ json/src/main/java/com/tjh/gsonFormat/RequestTwo.java
@@ -0,0 +1,29 @@
+package com.tjh.gsonFormat;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Created by tjh on 2019/3/6.
+ */
+@NoArgsConstructor
+@Data
+public class RequestTwo {
+
+    private List<RequestBean> request;
+
+    @NoArgsConstructor
+    @Data
+    public static class RequestBean {
+        private String amount;
+        private String checkCode;
+        private String invoiceCode;
+        private String invoiceNo;
+        private String paperDrewDate;
+        private String yzLevel;
+        private String tenantCode;
+        private String customerNo;
+    }
+}
